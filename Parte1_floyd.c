ca74bd8e1e2d9415df45d8bd666e381f35acd71e
sebastian <fernando.encina@fluxit.com.ar>
2020-07-10 19:31:48 -0300

    se completa algoritmo de floyd

--- Parte1_floyd.c
+++ Parte1_floyd.c
@@ -7,6 +7,7 @@ int main(){
 
 printf("\nCreando la matriz");
 
+//Se crea una matriz de adyacencia con todos los pesos de tamaño 16 X 16
     int matriz[nodos] [nodos] = { {0, 74, INF, INF, INF, INF, INF, INF,INF, 202, INF, INF, INF, INF, INF, 172},
 
                                         {74, 0, 108, 206, INF, INF, INF, INF, INF, 302, INF, 255, INF, INF, INF, 96},
@@ -40,4 +41,55 @@ printf("\nCreando la matriz");
                                         {172, 96, 76, 144, INF, 318, INF, 377, INF, 155, INF, 167, 215, INF, INF, 0}
 
     };
+    floyd(matriz);
+}
+
+void floyd (int matriz[nodos][nodos])
+{
+
+    //Creamos una nueva matriz para colocar los nuevos valores de distancias o usamos la misma que creamos????
+    int matriz_distancias[nodos][nodos], k, i, j;
+
+    //Inicializamos la matriz resultante que va a ser la de distancias
+    for (i = 0; i < nodos; i++)
+        for (j = 0; j < nodos; j++)
+            matriz_distancias[i][j] = matriz[i][j];
+
+    //Seleccionamos cada nodo intermedio (k)
+    for (k = 0; k < nodos; k++)
+    {
+        //Seleccionamos los nodos de inicio
+        for (i = 0; i < nodos; i++)
+        {
+            //Seleccionamos los nodos de destino
+            for (j = 0; j < nodos; j++)
+            {
+                //Verificamos si la distancia es menor a la existente
+                if (matriz_distancias[i][k] + matriz_distancias[k][j] < matriz_distancias[i][j])
+                    //Se actualiza la distancia
+                    matriz_distancias[i][j] = matriz_distancias[i][k] + matriz_distancias[k][j];
+            }
+        }
+    }
+    //Se coloca directamente la distancia de la posicion en la matriz [8][0] que corresponde a Chacabuco como origen y mar del plata como destino
+    printf("\nLa distancia minima entre las ciudades de Chacabuco y Mar del Plata es de: %d kms. \n", matriz_distancias[8][0]);
+    show_matriz_distancias(matriz_distancias);
+}
+
+void show_matriz_distancias(int matriz_distancias[nodos][nodos])
+{
+    int i;
+    for (i = 0; i < nodos; i++)
+    {
+        int j;
+        for (j = 0; j < nodos; j++)
+        {
+
+            if (matriz_distancias[i][j] == INF)
+                printf("%7s", "  INF  ");
+            else
+                printf (" %7d", matriz_distancias[i][j]);
+        }
+        printf("\n");
+    }
 }
